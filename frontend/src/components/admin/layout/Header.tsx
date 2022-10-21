66a40c18d6b2acd1dff1e28aeaa8532329b0a364
Badreddine Zatout <hb_zatout@esi.dz>
2022-10-21 23:27:55 +0100

    chore: change logo

--- frontend/src/components/admin/layout/Header.tsx
+++ frontend/src/components/admin/layout/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ImMenu } from 'react-icons/im';
+import { ImCart } from 'react-icons/im';
 
 import DropMenu from '@/components/admin/layout/DropMenu';
 import UnstyledLink from '@/components/links/UnstyledLink';
@@ -8,12 +8,13 @@ export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-dark py-2 text-light'>
       <div className='flex h-14 items-center justify-between px-5'>
-        <div className='flex items-center gap-x-5 text-xl font-bold'>
-          <UnstyledLink href='/' className='hover:text-secondary'>
-            <h1 className='text-xl'>e-shop</h1>
-          </UnstyledLink>
-          <ImMenu className='cursor-pointer hover:text-secondary' />
-        </div>
+        <UnstyledLink
+          href='/'
+          className='flex items-center gap-x-5 text-xl font-bold hover:text-secondary'
+        >
+          <ImCart className='h-10 w-10' />
+          <h1 className='text-2xl'>e-shop</h1>
+        </UnstyledLink>
         <nav className='flex items-center gap-x-5'>
           <input className='rounded-lg' type='text' placeholder='search here' />
           <DropMenu />
